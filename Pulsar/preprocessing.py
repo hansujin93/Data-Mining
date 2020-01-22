8826194d71b987177f47f5028f6ebcc4d36afd15
Sujin Han (Susie) <56803165+hansujin93@users.noreply.github.com>
2020-01-22 22:35:10 +0000

    Update preprocessing.py

--- Pulsar/preprocessing.py
+++ Pulsar/preprocessing.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-''' correlation matrix '''
+
+## Feature Selection ##
+''' 1) correlation based '''
 import numpy as np
 import math
 import pandas as pd
@@ -38,3 +40,15 @@ def corr_matrix(data):
             corr_m[i][j] = corr_by_features
     corr_m = pd.DataFrame(corr_m)
     return corr_m
+
+
+''' 2) permutation based '''
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from eli5.sklearn import PermutationImportance
+import eli5
+
+X_train, X_valid, y_train, y_valid = train_test_split(data_x, data_y, test_size=0.3, random_state=123)
+model = RandomForestClassifier(random_state=0).fit(X_train, y_train)
+perm = PermutationImportance(model, random_state=1).fit(X_valid, y_valid)
+eli5.show_weights(perm)
